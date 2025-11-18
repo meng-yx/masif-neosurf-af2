6ae15114d1ac98578bc2c6e2ce661692be8ffa17
Yanxiang Meng <meng.yanxiang@outlook.com>
2025-11-18 16:45:05 +0100

    run with singularity

--- masif/data/masif_ppi_search/data_prepare_one.sh
+++ masif/data/masif_ppi_search/data_prepare_one.sh
@@ -1,12 +1,18 @@
-masif_root=$(git rev-parse --show-toplevel)
+masif_neosurf_root=$(git rev-parse --show-toplevel)
+masif_root=$masif_neosurf_root/masif
 masif_source=$masif_root/source/
+docker_image=$masif_neosurf_root/masif-neosurf_v0.1.sif
 export PYTHONPATH=$PYTHONPATH:$masif_source
 PDB_ID=$(echo $1| cut -d"_" -f1)
 CHAIN1=$(echo $1| cut -d"_" -f2)
 CHAIN2=$(echo $1| cut -d"_" -f3)
-# Load your environment here. 
-python $masif_source/data_preparation/00-pdb_download.py $1 
-python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1 
-python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
-python $masif_source/data_preparation/04-masif_precompute.py masif_site $1
-python $masif_source/data_preparation/04-masif_precompute.py masif_ppi_search $1
+
+# Singularity bind paths - bind the entire repo root to make all paths accessible
+SINGULARITY_BIND="$masif_neosurf_root:$masif_neosurf_root"
+
+# Run Python scripts inside Singularity container
+singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/00-pdb_download.py $1 
+singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1 
+singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
+singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/04-masif_precompute.py masif_site $1
+singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/04-masif_precompute.py masif_ppi_search $1
