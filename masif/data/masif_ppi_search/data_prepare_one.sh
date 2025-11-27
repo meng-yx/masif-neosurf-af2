1a5db9b0e481749443d7e42b3995f3c465de724e
Thomas Philippe Rene Michel <tpmichel@jed1.jed.cluster>
2025-11-27 18:00:23 +0100

    Making_Af2_download_work

--- masif/data/masif_ppi_search/data_prepare_one.sh
+++ masif/data/masif_ppi_search/data_prepare_one.sh
@@ -10,8 +10,11 @@ CHAIN2=$(echo $1| cut -d"_" -f3)
 # Singularity bind paths - bind the entire repo root to make all paths accessible
 SINGULARITY_BIND="$masif_neosurf_root:$masif_neosurf_root"
 
-# Run Python scripts inside Singularity container
-singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/00-pdb_download.py $1 
+# Run Python scripts inside Singularity container from the masif/source directory
+# This ensures relative paths in masif_opts resolve correctly
+#singularity exec --bind $SINGULARITY_BIND $docker_image python 00a-pdb_to_af2.py $1
+
+#singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/00-pdb_download.py $1 
 singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1 
 singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
 singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/04-masif_precompute.py masif_site $1
