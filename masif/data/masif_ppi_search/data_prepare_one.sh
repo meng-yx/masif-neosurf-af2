d0f7cc1e8e1a426b5be3a00c32f6b27dd72fe537
Yanxiang Meng <meng.yanxiang@outlook.com>
2025-12-02 15:34:19 +0100

    Updated data_prepare.slurm workflow for AF2 models

--- masif/data/masif_ppi_search/data_prepare_one.sh
+++ masif/data/masif_ppi_search/data_prepare_one.sh
@@ -10,12 +10,17 @@ CHAIN2=$(echo $1| cut -d"_" -f3)
 # Singularity bind paths - bind the entire repo root to make all paths accessible
 SINGULARITY_BIND="$masif_neosurf_root:$masif_neosurf_root"
 
+# The new AF2 download script uses conda environment
+eval "$(conda shell.bash hook)"
+conda activate MaSIF
+python $masif_source/data_preparation/00a-PDB_to_AF2.py $1 \
+     --pdb-out-dir ./data_preparation/00-raw_pdbs/ \
+     --rmsd-out-dir ./data_preparation/00a-rmsd/ \
+     --temp-dir ./data_preparation/temp/
+
 # Run Python scripts inside Singularity container from the masif/source directory
 # This ensures relative paths in masif_opts resolve correctly
-#singularity exec --bind $SINGULARITY_BIND $docker_image python 00a-pdb_to_af2.py $1
-
-#singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/00-pdb_download.py $1 
-singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1 
+singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1
 singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
-singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/04-masif_precompute.py masif_site $1
-singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/04-masif_precompute.py masif_ppi_search $1
+# singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/04-masif_precompute.py masif_site $1
+# singularity exec --bind $SINGULARITY_BIND $docker_image python $masif_source/data_preparation/04-masif_precompute.py masif_ppi_search $1
