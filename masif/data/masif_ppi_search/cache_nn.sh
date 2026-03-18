3de1d9df4fbbf8c0305bef78cede002ad66b3f10
Yanxiang Meng <meng.yanxiang@outlook.com>
2026-03-18 15:59:06 +0100

    Small test 1.2 - cache_nn with 5to1 negatives and pooled negative sampling

--- masif/data/masif_ppi_search/cache_nn.sh
+++ masif/data/masif_ppi_search/cache_nn.sh
@@ -1,5 +1,11 @@
-masif_root=$(git rev-parse --show-toplevel)
-masif_source=$masif_root/masif/source/
-masif_data=$masif_root/masif/data/
-export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
-python3 $masif_source/masif_ppi_search/masif_ppi_search_cache_training_data.py $1
+masif_neosurf_root=$(git rev-parse --show-toplevel)
+masif_root=$masif_neosurf_root/masif
+masif_source=$masif_root/source/
+docker_image=$masif_neosurf_root/masif-neosurf_v0.1.sif
+export PYTHONPATH=$PYTHONPATH:$masif_source
+
+# Singularity bind paths - bind the entire repo root to make all paths accessible
+SINGULARITY_BIND="$masif_neosurf_root:$masif_neosurf_root"
+
+singularity exec --bind $SINGULARITY_BIND $docker_image python3 $masif_source/masif_ppi_search/masif_ppi_search_cache_training_data.py \
+    nn_models.sc05.all_feat.custom_params_mixed_5to1
