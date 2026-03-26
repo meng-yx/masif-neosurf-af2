03ac5b155c05837d4e55cfb0f82d8e775a87d416
Yanxiang Meng <meng.yanxiang@outlook.com>
2026-03-26 16:16:22 +0100

    Added comments on what each neg_mix mean

--- masif/data/masif_ppi_search/nn_models/sc05/all_feat/custom_params_baseline_1to1.py
+++ masif/data/masif_ppi_search/nn_models/sc05/all_feat/custom_params_baseline_1to1.py
@@ -6,9 +6,9 @@ custom_params["feat_mask"] = [1.0, 1.0, 1.0, 1.0, 1.0]
 
 # Baseline behavior (legacy-compatible).
 custom_params["neg_ratio"] = 1
-custom_params["neg_mix_cross_complex"] = 0.0
-custom_params["neg_mix_within_complex"] = 1.0
-custom_params["neg_mix_hard"] = 0.0
+custom_params["neg_mix_cross_complex"] = 0.0        # pair of patches coming from different complexes
+custom_params["neg_mix_within_complex"] = 1.0       # pair of patches coming from the same complex
+custom_params["neg_mix_hard"] = 0.0                 # pair of patches from within the same complex but are closest to the interface cutoff (just outside the interface region) - maybe not a good
 custom_params["enforce_diff_pdb_for_cross"] = True
 custom_params["hard_negative_topk"] = 200
 custom_params["neg_loss_weight"] = 1.0
